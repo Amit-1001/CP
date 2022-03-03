17d6bc9d74c05cecee2abb721dd41450d3d11ffb
Amit Nitnaware <amit.nitnaware03@gmail.com>
2022-03-03 21:03:20 +0530

    reverse String preserving space of string

---
+++ src/StringsProblem/reverseStringPreservingSpace.java
@@ -0,0 +1,52 @@
+package StringsProblem;
+
+import java.util.Scanner;
+
+public class reverseStringPreservingSpace {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = "Amit Nitnaware";
+        int start =0;
+        int last =str.length()-1;
+
+        char[] input = str.toCharArray();
+
+        char[] result = new char[str.length()];
+
+        System.out.println(String.valueOf(input));
+
+//        for(int i=0; i<str.length(); i++){
+//            if(input[i]!=' '){
+//                if(input[last]==' ') {
+//                last--;
+//                }
+//                result[i] = input[last];
+//                last--;
+//
+//                }else{
+//                result[i] = ' ';
+//            }
+//        }
+
+        while (start<=str.length()-1){
+            if(input[start]!=' '){
+                if(input[last]==' '){
+                    last--;
+                }
+                result[start] = input[last];
+                last--;
+            }else{
+                result[start] = ' ';
+            }
+            start++;
+        }
+
+        System.out.println(String.valueOf(result));
+
+    }
+
+
+
+
+
+}
